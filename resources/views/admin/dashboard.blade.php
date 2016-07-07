9845706a7636e8dedc7b26ee0f61a8f0270495c0
Manuel <manuellm@ymail.com>
2016-07-07 18:31:40 -0500

    prueba github

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('admin.main')
 @section('title', 'TrackFace Administrador')
 @section('content')
-
+PRUEBA DE GITHUB
 @endsection
--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title')</title>
+        
         <!-- Global stylesheets -->
         <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
         <link href="{{asset('plugin/limitless/assets/css/icons/icomoon/styles.css')}}" rel="stylesheet" type="text/css">
@@ -28,7 +29,7 @@
         <script type="text/javascript" src="{{asset('plugin/limitless/assets/js/plugins/ui/moment/moment.min.js')}}"></script>
         <script type="text/javascript" src="{{asset('plugin/limitless/assets/js/plugins/pickers/daterangepicker.js')}}"></script>
         <script type="text/javascript" src="{{asset('plugin/limitless/assets/js/core/app.js')}}"></script>
-        <script type="text/javascript" src="{{asset('plugin/limitless/assets/js/pages/dashboard.js')}}"></script>
+        <!--<script type="text/javascript" src="{{asset('plugin/limitless/assets/js/pages/dashboard.js')}}"></script>-->
         <!-- /theme JS files -->
     </head>
     <body>
